77d06eb15d3c1de4901ced47e05440984fff039f
warndarkahead <ayush.it.21023@recb.ac.in>
2024-01-09 23:53:53 +0530

    Selection Sort

--- 1.cpp
+++ 1.cpp
@@ -4,38 +4,33 @@
 
 using namespace std;
 
-int oddNum(vector<int> arr){
-    int s = 0;
-    int e = arr.size() -1;
-    int mid = s + (e-s)/2;
-    while (s<=e)
-    {
-        if(s==e){
-            return s;
-        }
-        if(mid%2==0){
-            if(arr[mid]==arr[mid+1]){
-                s = mid+2;
-            }
-            else{
-                e = mid;
-            }
-        }
-        else{
-            if(arr[mid-1]==arr[mid]){
-                s = mid + 1;
-            }
-            else{
-                e = mid-1;
-            }
-        }
-        mid = s + (e-s)/2;
-    }
-    return -1;
-}
 
-int main(){
-    vector<int> arr{1,1,2,2,3,3,4,4,5};
+
+int main(){     
+    vector<int> arr{5,4,3,2,1};
+    int n = 5;
+    //selection sort
+
+    for(int i = 0;i < n-1;i++){
+       int minIndex = i; 
+       for (int j = i+1; j < n; j++)
+       {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+
+       }
+        //swap 
+        swap(arr[i],arr[minIndex]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     
-    cout << oddNum(arr);
+
+    return 0;
 }
