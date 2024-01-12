bd4fe336c569f68a887eab67fcb3c2f0870d19e5
warndarkahead <ayush.it.21023@recb.ac.in>
2024-01-12 21:42:57 +0530

    Sort using algorithm library

--- 1.cpp
+++ 1.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<limits.h>
+#include<algorithm>
 
 using namespace std;
 
@@ -10,34 +11,9 @@ int main(){
     vector<int> arr{10,1,7,6,14,9};
     int n = arr.size();
 
-    //Insertion Sort
-
-    for (int round = 1; round < n; round++)
-    {
-        //Step1 fetch
-        int val = arr[round];
-
-        //Step2 Compare
-        int j = round  -1;
-        for (; j >= 0; j--)
-        {
-            if (arr[j]>val)
-            {
-                //Step3 shift
-                arr[j+1] = arr[j];
-            }
-            else{
-                //rukna hai
-                break; 
-            }            
-
-        }
-        //Step4 Copy
-
-        arr[j+1] = val;
-        
-    }
+        sort(arr.begin(),arr.end());
     
+
     //printing
     for (int i = 0; i < n; i++)
     {
