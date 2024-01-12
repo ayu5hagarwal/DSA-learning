ae4b9075729b9215b597d67680fa83085ef97d01
warndarkahead <ayush.it.21023@recb.ac.in>
2024-01-12 22:48:53 +0530

    Solved bug in binary search which is not having return while calling recusrion

--- 1.cpp
+++ 1.cpp
@@ -1,27 +1,27 @@
-#include<iostream>
-#include<vector>
-#include<limits.h>
-#include<algorithm>
-
+#include <iostream>
 using namespace std;
 
-
-
-int main(){     
-    vector<int> arr{10,1,7,6,14,9};
-    int n = arr.size();
-
-        sort(arr.begin(),arr.end());
-    
-
-    //printing
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
+int binarySearch(int arr[], int left, int right, int x) {
+    if (right >= left) {
+        int mid = (left + right) / 2;
+        if (arr[mid] == x)
+            return mid;
+        else if (arr[mid] > x)
+           return binarySearch(arr, left, mid - 1, x);
+        else
+           return binarySearch(arr, mid + 1, right, x);
     }
+    return -1;
+}
 
-    cout << endl;
-    
-
+int main() {
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 10;
+    int result = binarySearch(arr, 0, n - 1, x);
+    if (result == -1)
+        cout << "Element not found.";
+    else
+        cout << "Element found at index " << result << endl;
     return 0;
 }
