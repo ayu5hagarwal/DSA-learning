4ff4e02ca2ab2375da6e042ac45356abeb108616
warndarkahead <ayush.it.21023@recb.ac.in>
2024-01-14 16:44:59 +0530

    This code tries to implement Bubble Sort.

--- 1.cpp
+++ 1.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void selectionSort(int arr[], int n) {
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
     for (int i = 0; i < n - 1; i++) {
         int minIndex = i;
         for (int j = i + 1; j < n; j++) {
