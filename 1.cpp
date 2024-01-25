6d407bded1370032ccf9c08b901b15c883339357
warndarkahead <ayush.it.21023@recb.ac.in>
2024-01-26 03:51:21 +0530

    Reverse an array using recursive function

--- 1.cpp
+++ 1.cpp
@@ -3,8 +3,21 @@
 
 using namespace std;
 
-void reversedArr(int originalArr[],int size){
-    reverse(originalArr,originalArr+size);
+//Recursion Method
+
+void reversedArr(int originalArr[],int i,int j){
+   if(i>=j) {
+    return;
+   }
+
+   int temp = originalArr[j];
+   originalArr[j] = originalArr[i];
+   originalArr[i] = temp;
+
+   reversedArr(originalArr,i+1,j-1) ;
+}
+
+void printArray(int originalArr[],int size){
     for (int i = 0; i < size; i++)
     {
         cout << originalArr[i] << " ";
@@ -15,6 +28,8 @@ void reversedArr(int originalArr[],int size){
 int main(){
     int originalArr[] = {1,2,3,4,5};
     int size = sizeof(originalArr)/sizeof(originalArr[0]);
-
-    reversedArr(originalArr,size);
+    int i = 0;
+    int j = size - 1;
+    reversedArr(originalArr,i,j);
+    printArray(originalArr,size);
 }
